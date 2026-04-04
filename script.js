14550d9c3f38178822576e0be352aedda4bc6a87
leorivero24 <leo.exerivero@gmail.com>
2026-04-04 20:59:31 -0300

    commit

--- script.js
+++ script.js
@@ -52,119 +52,9 @@ window.onscroll = function () {
     efectoHabilidades();
 }
 
-//validar formulario contacto
 
-// const idioma = document.documentElement.lang;
 
-// const mensajes = {
-//     es: {
-//         nombre: 'El nombre es obligatorio.',
-//         telefono: 'El teléfono debe tener 10 dígitos.',
-//         email: 'El email no es válido.',
-//         mensaje: 'El mensaje es obligatorio.',
-//         enviado: 'Mensaje enviado correctamente ✅',
-//         error: 'Error al enviar ❌'
-//     },
-//     en: {
-//         nombre: 'The name is required.',
-//         telefono: 'The phone must have 10 digits.',
-//         email: 'The email is not valid.',
-//         mensaje: 'The message is required.',
-//         enviado: 'Message sent successfully ✅',
-//         error: 'Error sending message ❌'
-//     }
-// };
 
-// function enviarMensaje() {
-
-//     const idioma = document.documentElement.lang;
-//     const msg = mensajes[idioma] || mensajes['es'];
-
-//     document.getElementById('nombreError').textContent = '';
-//     document.getElementById('telefonoError').textContent = '';
-//     document.getElementById('emailError').textContent = '';
-//     document.getElementById('mensajeError').textContent = '';
-
-//     const nombre = document.getElementById('nombre').value.trim();
-//     const telefono = document.getElementById('telefono').value.trim();
-//     const email = document.getElementById('email').value.trim();
-//     const mensaje = document.getElementById('mensaje').value.trim();
-
-//     let hayErrores = false;
-
-//     if (!nombre) {
-//         document.getElementById('nombreError').textContent = msg.nombre;
-//         hayErrores = true;
-//     }
-
-//     if (telefono.length !== 10) {
-//         document.getElementById('telefonoError').textContent = msg.telefono;
-//         hayErrores = true;
-//     }
-
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     if (!emailRegex.test(email)) {
-//         document.getElementById('emailError').textContent = msg.email;
-//         hayErrores = true;
-//     }
-
-//     if (!mensaje) {
-//         document.getElementById('mensajeError').textContent = msg.mensaje;
-//         hayErrores = true;
-//     }
-
-//     if (!hayErrores) {
-
-//         const btn = document.getElementById("enviarBtn");
-//         btn.disabled = true;
-//         btn.innerText = idioma === "en" ? "Sending..." : "Enviando...";
-
-//         const templateParams = {
-//             name: nombre,
-//             phone: telefono,
-//             email: email,
-//             message: mensaje
-//         };
-
-//         emailjs.send("service_f1yu2k9", "template_zr0g188", templateParams)
-//             .then(function () {
-
-//                 const resultadoDiv = document.getElementById('resultado');
-
-//                 resultadoDiv.innerHTML = `<h3>${msg.enviado}</h3>`;
-
-//                 setTimeout(() => {
-//                     resultadoDiv.classList.add("fade-out");
-//                 }, 2500);
-
-//                 setTimeout(() => {
-//                     resultadoDiv.innerHTML = "";
-//                     resultadoDiv.classList.remove("fade-out");
-//                 }, 3000);
-
-//                 document.getElementById('nombre').value = '';
-//                 document.getElementById('telefono').value = '';
-//                 document.getElementById('email').value = '';
-//                 document.getElementById('mensaje').value = '';
-
-//                 setTimeout(() => {
-//                     resultadoDiv.innerHTML = "";
-
-//                     btn.disabled = false;
-//                     btn.innerText = idioma === "en" ? "SEND MESSAGE" : "ENVIAR MENSAJE";
-
-//                 }, 3000);
-
-//             }, function (error) {
-//                 console.log(error);
-
-//                 btn.disabled = false;
-//                 btn.innerText = "SEND MESSAGE";
-
-//                 alert("❌ Error al enviar");
-//             });
-//     }
-// }
 
 
 
@@ -172,7 +62,7 @@ const mensajes = {
     es: {
         nombre: 'El nombre es obligatorio.',
         telefono: 'El teléfono debe tener 10 dígitos.',
-        email: 'El email no es válido.',
+        email: 'El correo electrónico no es válido.',
         mensaje: 'El mensaje es obligatorio.',
         enviado: 'Mensaje enviado correctamente ✅',
         error: 'Error al enviar ❌',
